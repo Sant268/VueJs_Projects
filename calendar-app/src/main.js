36f544f7f3f3343bc09ef5a519270d7940539b51
Kunal Booch <kunalbooch@hotmail.com>
2020-05-26 21:54:59 +0530

    Update main.js

--- calendar-app/src/main.js
+++ calendar-app/src/main.js
@@ -9,13 +9,7 @@ Vue.config.productionTip = false
 Vue.use(VueTextareaAutosize)
 
 firebase.initializeApp({
-    apiKey: "AIzaSyCt6KXMERKZ42y4sEG2t7BbFaJCldKXJ6s",
-     authDomain: "vue-calendar-5863e.firebaseapp.com",
-     databaseURL: "https://vue-calendar-5863e.firebaseio.com",
-     projectId: "vue-calendar-5863e",
-     storageBucket: "vue-calendar-5863e.appspot.com",
-     messagingSenderId: "179844507228",
-     appId: "1:179844507228:web:a2749c4ddf8960bd4f2899"
+   //private
 });
 
 export const db = firebase.firestore();
